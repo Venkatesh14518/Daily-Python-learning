1efa7f0f02f84c6dbf6d853f799f25d800cbc7c5
Venky <venkatesh14518@gmail.com>
2025-11-24 17:16:55 +0530

    Day 4 - Logical operators (NOT, OR, AND)

---
+++ Week 1/07. Logical basics.py
@@ -0,0 +1,24 @@
+# Logical Operation_1
+# Logical Operation NOT
+
+a = True      # variable a stores the boolean value True
+b = False     # variable b stores the boolean value False
+
+# print the value of not of a and not of b
+print(not a)  # NOT flips the value; since a is True, not a becomes False
+print(not b)  # NOT flips the value; since b is False, not b becomes True
+
+
+# Logical Operation_2
+# Logical Operation OR
+
+# print the value of a OR b
+print(a or b)  # OR returns True if at least one value is True; since a is True, result is True
+
+
+# Logical Operation_#
+# Logical Operation AND
+
+# print the value of a AND b
+print(a and b)  # AND returns True only if both values are True; a is True but b is False, so result is False
+
