4c8627a60d7fd999f2128867df2cec97a3939b43
Venky <venkatesh14518@gmail.com>
2025-12-02 23:04:35 +0530

    Day 12 - Implement even number loop without if-else

---
+++ Week 2/09. Evens_no 'if'.py
@@ -0,0 +1,6 @@
+# Exercise
+
+# Write a program to print the even numbers from 0 to 100 using for loop and without using if-else
+
+for num in range(0, 101, 2):     # start at 0, go to 100, step by 2 → gives only even numbers
+    print(num)                   # print each even number
